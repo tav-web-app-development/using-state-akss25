47cc822d9abb2fb7f87197b4be07b238835e682f
Kudumula Akshith Yadav <akshithyadav111@gmail.com>
2024-03-05 19:24:21 -0500

    second commit

--- src/components/ProductCardContainer.jsx
+++ src/components/ProductCardContainer.jsx
@@ -1,4 +1,4 @@
-import ProductCard from "./ProductCard";
+import ProductCard from "./ProductCard.jsx";
 
 export default function ProductCardContainer({ products }) {
   const productsJsx = products.map((product) => (
